302530170415fd3ce6b8a25d110dc031839c1aad
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-03-18 16:01:24 +0100

    Creato server + applciation.properties

---
+++ server/src/main/java/it/iad/relazioni/RelazioniApplication.java
@@ -0,0 +1,13 @@
+package it.iad.relazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelazioniApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/it/iad/relazioni/RelazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad.relazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
